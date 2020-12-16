b7b5620680e3934ee10a85663497006e04aaf243
Ben Turney <btatvt11@gmail.com>
2020-12-16 13:42:54 -0500

    Day 2 Part 2 complete

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -8,6 +8,9 @@ fun main() {
 
     val part1Count = data.count { it.validPart1 }
     println("Part One's answer is $part1Count")
+
+    val part2Count = data.count { it.validPart2 }
+    println("Part Two's answer is $part2Count")
 }
 
 object Day02 {
@@ -29,4 +32,6 @@ data class PasswordRecord(val range: IntRange, val letter: Char, val password: S
     }
 
     val validPart1 = password.count { it == letter } in range
+
+    val validPart2 = (password[range.first - 1] == letter) xor (password[range.last - 1] == letter)
 }
