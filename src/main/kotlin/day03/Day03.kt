ef5ea39a6ee9a578d0ad3ab8e5fcd2a2afa39bb1
Ben Turney <btatvt11@gmail.com>
2020-12-17 09:12:49 -0500

    Advent of Code Day 3 Part 1

---
+++ src/main/kotlin/day03/Day03.kt
@@ -0,0 +1,43 @@
+package day03
+
+fun main() {
+    val skiSlope = Files.readFileAsList("src/main/resources/day03.txt")
+    Day03(skiSlope).apply {
+        val part1Answer = solvePart1()
+        println("Part One's answer is $part1Answer")
+    }
+}
+
+internal class Day03(private val skiSlope: List<String>) {
+
+    private val width = skiSlope.first().length
+    private val height = skiSlope.size
+
+    // Override the + operator for Pair
+    // This isn't strictly necessary but it makes the code a bit cleaner below I think
+    private operator fun Pair<Int, Int>.plus(that: Pair<Int, Int>) =
+        Pair(this.first + that.first, this.second + that.second)
+
+    /*
+    Override the contains method for List to check for a tree
+    Represented by a '#'
+    Because the pattern of trees repeats we can use the modulus operator
+    to calculate our index given the true width
+    */
+    private operator fun List<String>.contains(location: Pair<Int, Int>) =
+        this[location.second][location.first % width] == '#'
+
+    // Calculate the coordinates of our path given a slope
+    private fun getPath(slope: Pair<Int, Int>): Sequence<Pair<Int, Int>> =
+        generateSequence(Pair(0, 0)) { prev ->
+            (prev + slope).takeIf { next -> next.second < height }
+        }
+
+    private fun countTreesOnSlope(slope: Pair<Int, Int>) =
+        getPath(slope).count { it in skiSlope }
+
+    fun solvePart1(): Int = countTreesOnSlope(3 to 1)
+}
+
+
+
