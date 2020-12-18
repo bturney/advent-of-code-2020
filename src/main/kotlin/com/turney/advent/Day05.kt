b7129d74ed71d35c2dfbbf2a07364ebd03394632
Ben Turney <btatvt11@gmail.com>
2020-12-18 16:19:27 -0500

    Replace deprecated max method with maxOrNull

--- src/main/kotlin/com/turney/advent/Day05.kt
+++ src/main/kotlin/com/turney/advent/Day05.kt
@@ -8,13 +8,13 @@ class Day05(private val input: List<String>) {
     private fun seatId(pattern: String) =
         pattern.map { if (it in setOf('B', 'R')) '1' else '0' }.joinToString("").toInt(2)
 
-    fun solvePart1() = input.map { seatId(it) }.max()
+    fun solvePart1() = input.map { seatId(it) }.maxOrNull() ?: throw IllegalStateException("No answer")
 
     fun solvePart2() =
         input.map { seatId(it) }
             .sorted()
             .zipWithNext() // creates a list of pairs for each adjacent element
-            .first {it.second - it.first != 1} // take the first pair that is not adjacent
+            .first { it.second - it.first != 1 } // take the first pair that is not adjacent
             .first + 1 // take the first element of the pair and add 1 to get our seat
 
 
